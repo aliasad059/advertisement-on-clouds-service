2f6a4fe489b7ad63056ea2957ced348ff18c6951
Ali <aliasad059>
2022-11-08 21:13:14 +0330

    fix: fix imports

--- services/rabbitmq_receiver/src/main.py
+++ services/rabbitmq_receiver/src/main.py
@@ -1,5 +1,5 @@
 from config import rabbitmq_config
-from services.rabbitmq_receiver.rabbitmq_receiver import RabbitMQReceiver
+from rabbitmq_receiver import RabbitMQReceiver
 
 if __name__ == '__main__':
     rabbitmq_receiver = RabbitMQReceiver(rabbitmq_config['AMQP_URL'])
