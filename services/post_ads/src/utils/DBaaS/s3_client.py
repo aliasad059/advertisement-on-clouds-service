d2b7f612f3246c2b04712c6b1dc644532e442471
Ali <aliasad059>
2022-11-14 01:07:01 +0330

    fix: create s3 client to get file url

--- services/post_ads/src/utils/DBaaS/s3_client.py
+++ services/post_ads/src/utils/DBaaS/s3_client.py
@@ -4,14 +4,21 @@ from botocore.exceptions import ClientError
 
 class S3Connector():
     def __init__(self, endpoint_url, access_key, secret_key, bucket_name):
-        self.s3 = boto3.resource(
+        self.s3_client = boto3.client(
+            's3',
+            endpoint_url=endpoint_url,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
+        )
+
+        self.bucket_name = bucket_name
+        self.bucket = boto3.resource(
                 's3',
                 endpoint_url=endpoint_url,
                 aws_access_key_id=access_key,
                 aws_secret_access_key=secret_key
-            )
-        self.bucket_name = bucket_name
-        self.bucket = self.s3.Bucket(bucket_name)
+            ).Bucket(bucket_name)
+        
         print("Connected to the S3 server successfully")
         
     def upload_file(self, image_file, object_name):
@@ -35,7 +42,7 @@ class S3Connector():
             Returns the url of the file
         """
         try:
-            response = self.s3.generate_presigned_url(
+            response = self.s3_client.generate_presigned_url(
                 'get_object',
                 Params={
                     'Bucket': self.bucket_name,
